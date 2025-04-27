51644ad0e57db13a1423922920b394dfd7377375
ToChoSK <tchovancak10@gmail.com>
2025-04-27 13:44:34 +0200

    Fix: Package scripts update

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export', // PRIDANÉ - statický export
+  basePath: '/gromanka', // PRIDANÉ - názov tvojho repozitára
+  assetPrefix: '/gromanka/', // PRIDANÉ - správne odkazy
 }
 
 export default nextConfig
